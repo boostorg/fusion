cd23b2b0390d286fc15131482630582d1b0f2e4f
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2014-02-11 23:39:42 +0100

    EDITORIAL: Fixes typos.

--- include/boost/fusion/adapted/struct/auto_adapt_struct.hpp
+++ include/boost/fusion/adapted/struct/auto_adapt_struct.hpp
@@ -17,12 +17,12 @@
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
 
 #define BOOST_FUSION_AUTO_ADAPT_STRUCT_FILLER(r, NAME, ATTRIBUTE)               \
-     (BOOST_TYPEOF(NAME::ATTRIBUTE), ATTRIBUTE)                                 \
+     (BOOST_TYPEOF(NAME::ATTRIBUTE), ATTRIBUTE)
 
 #define BOOST_FUSION_AUTO_ADAPT_STRUCT(NAME, ATTRIBUTES)                        \
     BOOST_FUSION_ADAPT_STRUCT(NAME,                                             \
         BOOST_PP_SEQ_FOR_EACH(                                                  \
-          BOOST_FUSION_AUTO_ADAPT_STRUCT_FILLER, NAME, ATTRIBUTES)              \
+            BOOST_FUSION_AUTO_ADAPT_STRUCT_FILLER, NAME, ATTRIBUTES)            \
         )
 
 #endif
