74a5d8804c65073cd3bc86c9bcfbe9c42a43b0d1
Joel de Guzman <djowel@gmail.com>
2014-05-01 13:44:45 +0800

    reverting support for ref pending review

--- include/boost/fusion/support.hpp
+++ include/boost/fusion/support.hpp
@@ -1,7 +1,7 @@
 /*=============================================================================
     Copyright (c) 2001-2011 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_SUPPORT_10022005_0545)
@@ -21,6 +21,5 @@
 #include <boost/fusion/support/deduce_sequence.hpp>
 #include <boost/fusion/support/unused.hpp>
 #include <boost/fusion/support/as_const.hpp>
-#include <boost/fusion/support/ref.hpp>
 
 #endif
