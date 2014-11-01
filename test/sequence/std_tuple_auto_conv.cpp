7ef4b03249108d48fe7aa9d4d50302f0e8a6eb96
Kohei Takahashi <flast@flast.jp>
2014-11-01 22:12:11 +0900

    Fix typo/missing copyright notice, reported by Boost Inspection Report.

--- test/sequence/std_tuple_auto_conv.cpp
+++ test/sequence/std_tuple_auto_conv.cpp
@@ -1,3 +1,9 @@
+/*=============================================================================
+    Copyright (c) 2008 Joel de Guzman
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
 #include <boost/config.hpp>
 
 #if !defined(BOOST_NO_CXX11_HDR_TUPLE) && \
