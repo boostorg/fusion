d9370039621553844ded593b7f35514028b8d6c6
Kohei Takahashi <flast@flast.jp>
2014-11-09 15:27:54 +0900

    Prepare implementing c++11 variadic based fusion::vector.

--- include/boost/fusion/container/vector/vector.hpp
+++ include/boost/fusion/container/vector/vector.hpp
@@ -7,14 +7,21 @@
 #ifndef FUSION_VECTOR_11052014_1625
 #define FUSION_VECTOR_11052014_1625
 
-#include <boost/config.hpp>
 #include <boost/fusion/support/config.hpp>
+#include <boost/fusion/container/vector/detail/config.hpp>
 #include <boost/fusion/container/vector/vector_fwd.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Without variadics, we will use the PP version
 ///////////////////////////////////////////////////////////////////////////////
+#if !defined(BOOST_FUSION_HAS_VARIADIC_VECTOR)
 # include <boost/fusion/container/vector/detail/cpp03/vector.hpp>
+#else
 
+///////////////////////////////////////////////////////////////////////////////
+// C++11 interface
+///////////////////////////////////////////////////////////////////////////////
+
+#endif
 #endif
 
--- include/boost/fusion/container/vector/vector_fwd.hpp
+++ include/boost/fusion/container/vector/vector_fwd.hpp
@@ -9,11 +9,24 @@
 
 #include <boost/config.hpp>
 #include <boost/fusion/support/config.hpp>
+#include <boost/fusion/container/vector/detail/config.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 // With no variadics, we will use the C++03 version
 ///////////////////////////////////////////////////////////////////////////////
+#if !defined(BOOST_FUSION_HAS_VARIADIC_VECTOR)
 # include <boost/fusion/container/vector/detail/cpp03/vector_fwd.hpp>
+#else
 
+///////////////////////////////////////////////////////////////////////////////
+// C++11 interface
+///////////////////////////////////////////////////////////////////////////////
+namespace boost { namespace fusion
+{
+    template <typename ...T>
+    struct vector;
+}}
+
+#endif
 #endif
 
