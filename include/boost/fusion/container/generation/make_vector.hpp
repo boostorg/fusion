c77f84749f58550d70291546f5fb418ac459bb74
Kohei Takahashi <flast@flast.jp>
2014-11-11 11:16:30 +0900

    Implement variadic generators for fusion::vector.

--- include/boost/fusion/container/generation/make_vector.hpp
+++ include/boost/fusion/container/generation/make_vector.hpp
@@ -10,7 +10,37 @@
 #include <boost/fusion/support/config.hpp>
 #include <boost/fusion/container/vector/vector.hpp>
 
+#if !defined(BOOST_FUSION_HAS_VARIADIC_VECTOR)
 # include <boost/fusion/container/generation/detail/pp_make_vector.hpp>
+#else
 
+///////////////////////////////////////////////////////////////////////////////
+// C++11 variadic interface
+///////////////////////////////////////////////////////////////////////////////
+
+#include <boost/fusion/support/detail/as_fusion_element.hpp>
+
+namespace boost { namespace fusion
+{
+    namespace result_of
+    {
+        template <typename ...T>
+        struct make_vector
+        {
+            typedef vector<typename detail::as_fusion_element<T>::type...> type;
+        };
+    }
+
+    template <typename ...T>
+    BOOST_FUSION_GPU_ENABLED
+    inline vector<typename detail::as_fusion_element<T>::type...>
+    make_vector(T const&... arg)
+    {
+        return vector<typename detail::as_fusion_element<T>::type...>(arg...);
+    }
+ }}
+
+
+#endif
 #endif
 
