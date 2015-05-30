1e7e3f67f0248b3da9b3e579d7f72e948a3329ed
Kohei Takahashi <flast@flast.jp>
2015-05-30 11:35:14 +0900

    Retrive workaround

--- include/boost/fusion/support/detail/is_constructible.hpp
+++ include/boost/fusion/support/detail/is_constructible.hpp
@@ -20,15 +20,35 @@ namespace boost { namespace fusion { namespace detail
         typedef char yes_type;
         struct no_type { char xx[2]; };
 
+#ifdef BOOST_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundefined-inline"
+#endif
+#if BOOST_WORKAROUND(BOOST_MSVC, BOOST_TESTED_AT(1800))
         template <typename T_, typename ...A_>
         BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
         static yes_type
         check(T_ const& = T_(boost::declval<A_>()...));
+#else
+        template <std::size_t>
+        struct wrap
+        {
+            wrap(int);
+        };
+
+        template <typename T_, typename ...A_>
+        BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
+        static yes_type
+        check(wrap<sizeof(T_(boost::declval<A_>()...))>);
+#endif
 
         template <typename, typename ...>
         BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
         static no_type
         check(...);
+#ifdef BOOST_CLANG
+#pragma clang diagnostic pop
+#endif
 
         typedef is_constructible type;
         BOOST_STATIC_CONSTANT(bool, value = sizeof(yes_type) == sizeof(check<T, A...>(0)));
