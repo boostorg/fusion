c30fabf98f68039b698e2e4417e281b881150cde
Kohei Takahashi <flast@flast.jp>
2015-08-30 17:13:36 +0900

    Apply 7fdb320 change into C++98 interface.

--- test/sequence/deque_is_constructible.cpp
+++ test/sequence/deque_is_constructible.cpp
@@ -5,6 +5,9 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 
+#include <boost/config.hpp>
+
+#ifndef BOOST_NO_CXX11_HDR_TYPE_TRAITS
 #include <type_traits>
 #include <boost/fusion/include/deque.hpp>
 
@@ -14,4 +17,5 @@ struct Dummy { };
 // Make sure deque's constructor is SFINAE-friendly.
 static_assert(!std::is_constructible<boost::fusion::deque<int>, Dummy const&>::value, "");
 
-int main() { }
+#endif
+
