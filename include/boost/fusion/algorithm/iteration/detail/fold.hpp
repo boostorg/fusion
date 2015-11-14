1c05cfc795a2401a317529711a1f34d6a554abfd
Kohei Takahashi <flast@flast.jp>
2015-11-14 20:34:41 +0900

    Regenerate fusion::fold family.

--- include/boost/fusion/algorithm/iteration/detail/fold.hpp
+++ include/boost/fusion/algorithm/iteration/detail/fold.hpp
@@ -9,6 +9,8 @@
 ==============================================================================*/
 #include <boost/preprocessor/cat.hpp>
 
+#define FUSION_HASH #
+
 #ifdef BOOST_FUSION_REVERSE_FOLD
 #   ifdef BOOST_FUSION_ITER_FOLD
 #       define BOOST_FUSION_FOLD_NAME reverse_iter_fold
@@ -42,10 +44,18 @@
 #   define BOOST_FUSION_FOLD_IMPL_INVOKE_IT_TRANSFORM(IT) fusion::deref(IT)
 #endif
 
-#if BOOST_WORKAROUND(BOOST_MSVC, < 1500)
-#   define BOOST_FUSION_FOLD_IMPL_ENABLER(T) void
+#if (defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES))
+FUSION_HASH if BOOST_WORKAROUND BOOST_PREVENT_MACRO_SUBSTITUTION (BOOST_MSVC, < 1500)
+FUSION_HASH     define BOOST_FUSION_FOLD_IMPL_ENABLER(T) void
+FUSION_HASH else
+FUSION_HASH     define BOOST_FUSION_FOLD_IMPL_ENABLER(T) typename T::type
+FUSION_HASH endif
 #else
-#   define BOOST_FUSION_FOLD_IMPL_ENABLER(T) typename T::type
+#   if BOOST_WORKAROUND(BOOST_MSVC, < 1500)
+#       define BOOST_FUSION_FOLD_IMPL_ENABLER(T) void
+#   else
+#       define BOOST_FUSION_FOLD_IMPL_ENABLER(T) typename T::type
+#   endif
 #endif
 
 namespace boost { namespace fusion
@@ -53,9 +63,16 @@ namespace boost { namespace fusion
     namespace detail
     {
         template<int SeqSize, typename It, typename State, typename F, typename = void
-#if BOOST_WORKAROUND(BOOST_MSVC, < 1500)
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH if BOOST_WORKAROUND BOOST_PREVENT_MACRO_SUBSTITUTION (BOOST_MSVC, < 1500)
+#endif
+#if BOOST_WORKAROUND(BOOST_MSVC, < 1500) || \
+    (defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES))
           // Dirty hack: those compilers cannot choose exactly one partial specialization.
           , bool = SeqSize == 0
+#endif
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH endif
 #endif
         >
         struct BOOST_PP_CAT(result_of_it_,BOOST_FUSION_FOLD_NAME)
@@ -64,8 +81,15 @@ namespace boost { namespace fusion
         template<typename It, typename State, typename F>
         struct BOOST_PP_CAT(result_of_it_,BOOST_FUSION_FOLD_NAME)<0,It,State,F
           , typename boost::enable_if_has_type<BOOST_FUSION_FOLD_IMPL_ENABLER(State)>::type
-#if BOOST_WORKAROUND(BOOST_MSVC, < 1500)
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH if BOOST_WORKAROUND BOOST_PREVENT_MACRO_SUBSTITUTION (BOOST_MSVC, < 1500)
+#endif
+#if BOOST_WORKAROUND(BOOST_MSVC, < 1500) || \
+    (defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES))
           , true
+#endif
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH endif
 #endif
           >
         {
@@ -75,17 +99,35 @@ namespace boost { namespace fusion
         template<int SeqSize, typename It, typename State, typename F>
         struct BOOST_PP_CAT(result_of_it_,BOOST_FUSION_FOLD_NAME)<SeqSize,It,State,F
           , typename boost::enable_if_has_type<
-#if BOOST_WORKAROUND(BOOST_MSVC, == 1500)
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH if BOOST_WORKAROUND BOOST_PREVENT_MACRO_SUBSTITUTION (BOOST_MSVC, == 1500)
+#endif
+#if BOOST_WORKAROUND(BOOST_MSVC, == 1500) || \
+    (defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES))
                 // Following SFINAE enables to avoid MSVC 9's partial specialization
                 // ambiguous bug but MSVC 8 don't compile, and moreover MSVC 8 style
                 // workaround won't work with MSVC 9.
                 typename boost::disable_if_c<SeqSize == 0, State>::type::type
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH else
+                BOOST_FUSION_FOLD_IMPL_ENABLER(State)
+#endif
 #else
                 BOOST_FUSION_FOLD_IMPL_ENABLER(State)
 #endif
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH endif
+#endif
             >::type
-#if BOOST_WORKAROUND(BOOST_MSVC, < 1500)
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH if BOOST_WORKAROUND BOOST_PREVENT_MACRO_SUBSTITUTION (BOOST_MSVC, < 1500)
+#endif
+#if BOOST_WORKAROUND(BOOST_MSVC, < 1500) || \
+    (defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES))
           , false
+#endif
+#if defined(__WAVE__) && defined(BOOST_FUSION_CREATE_PREPROCESSED_FILES)
+FUSION_HASH endif
 #endif
           >
           : BOOST_PP_CAT(result_of_it_,BOOST_FUSION_FOLD_NAME)<
@@ -249,3 +291,4 @@ namespace boost { namespace fusion
 #undef BOOST_FUSION_FOLD_IMPL_FIRST_IT_TRANSFORM
 #undef BOOST_FUSION_FOLD_IMPL_INVOKE_IT_META_TRANSFORM
 #undef BOOST_FUSION_FOLD_IMPL_INVOKE_IT_TRANSFORM
+#undef FUSION_HASH
