boostorg/fusion

commit 79d8e9d11c07099bdf1db0724e8fb1ee92e98920

Added to nested tests, and fixed C++11 vector copy-from-sequence

include/boost/fusion/container/vector/vector.hpp
@@ -33,13 +33,14 @@
 #include <boost/fusion/container/vector/detail/end_impl.hpp>
 #include <boost/fusion/sequence/intrinsic/size.hpp>
 #include <boost/fusion/sequence/intrinsic/begin.hpp>
+#include <boost/fusion/sequence/intrinsic/size.hpp>
 #include <boost/fusion/iterator/advance.hpp>
 #include <boost/fusion/iterator/deref.hpp>
 #include <boost/core/enable_if.hpp>
-#include <boost/mpl/int.hpp>
 #include <boost/mpl/bool.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/int.hpp>
 #include <boost/type_traits/is_same.hpp>
-#include <boost/type_traits/is_convertible.hpp>
 #include <boost/type_traits/remove_cv.hpp>
 #include <boost/type_traits/remove_reference.hpp>
 #include <cstddef>
@@ -66,15 +67,20 @@ namespace boost { namespace fusion
         template <typename T>
         struct pure : remove_cv<typename remove_reference<T>::type> {};
 
-        template <typename Sequence, typename This, int = result_of::size<This>::value>
-        struct is_convertible_to_first
-            : boost::is_convertible<Sequence, typename result_of::value_at_c<This, 0>::type>
-        {};
+        template<typename Sequence, typename This, typename Enable = void>
+        struct can_convert : mpl::false_ {};
 
-        template <typename Sequence, typename This>
-        struct is_convertible_to_first<Sequence, This, 0>
-            : mpl::false_
-        {};
+        template<typename Sequence, typename This>
+        struct can_convert<
+            Sequence
+          , This
+          , typename enable_if<traits::is_sequence<Sequence>>::type
+        >   : mpl::equal_to<
+                  fusion::result_of::size<Sequence>
+                , fusion::result_of::size<This>
+              >
+        {
+        };
 
         template <typename This, typename ...T>
         BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
@@ -90,9 +96,8 @@ namespace boost { namespace fusion
         BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
         inline from_sequence<
             typename lazy_enable_if_c<
-                (traits::is_sequence<typename remove_reference<Sequence>::type>::value &&
-                 !is_same<This, typename pure<Sequence>::type>::value &&
-                 !is_convertible_to_first<Sequence, This>::value)
+                (!is_same<This, typename pure<Sequence>::type>::value &&
+                 can_convert<typename pure<Sequence>::type, This>::value)
               , make_indices_from_seq<Sequence>
             >::type
         >

test/sequence/nest.hpp
@@ -7,6 +7,43 @@
 
 #include <utility>
 #include <boost/config.hpp>
+#include <boost/fusion/include/as_deque.hpp>
+#include <boost/fusion/include/as_list.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+
+template<typename C, template<typename> class As>
+void test_from_sequence_rvalue()
+{
+    typename As<C>::type dst((C()));
+    (void)dst;
+}
+
+template<typename C, template<typename> class As>
+void test_from_sequence_const_lvalue()
+{
+    C src;
+    typename As<C>::type dst(src);
+    (void)dst;
+}
+
+template<typename C, template<typename> class As>
+void test_from_sequence_lvalue()
+{
+    const C src;
+    typename As<C>::type dst(src);
+    (void)dst;
+}
+
+template<typename C, template<typename> class As>
+void test_from_sequence()
+{
+// the following tests do not work in all cases for C++03
+#if defined(BOOST_FUSION_HAS_VARIADIC_VECTOR)
+    test_from_sequence_rvalue<C, As>();
+    test_from_sequence_const_lvalue<C, As>();
+    test_from_sequence_lvalue<C, As>();
+#endif
+}
 
 template <typename C>
 void test_copy()
@@ -29,6 +66,10 @@ void test_move()
 template <typename C>
 void test_all()
 {
+// as_deque and as_list do not work in C++03 or C++11 mode
+//    test_from_sequence<C, boost::fusion::result_of::as_deque>();
+//    test_from_sequence<C, boost::fusion::result_of::as_list>();
+    test_from_sequence<C, boost::fusion::result_of::as_vector>();
     test_copy<C>();
 #if !defined(BOOST_NO_CXX11_RVALUE_REFERENCES)
     test_move<C>();
@@ -54,5 +95,23 @@ test()
     test_all<FUSION_SEQUENCE<FUSION_SEQUENCE<int, float>, int> >();
     test_all<FUSION_SEQUENCE<int, FUSION_SEQUENCE<int, float> > >();
     test_all<FUSION_SEQUENCE<int, FUSION_SEQUENCE<int, float>, float> >();
+
+    test_all<FUSION_SEQUENCE<FUSION_SEQUENCE<>, FUSION_SEQUENCE<> > >();
+    test_all<FUSION_SEQUENCE<FUSION_SEQUENCE<int>, FUSION_SEQUENCE<> > >();
+    test_all<FUSION_SEQUENCE<FUSION_SEQUENCE<>, FUSION_SEQUENCE<int> > >();
+    test_all<
+        FUSION_SEQUENCE<FUSION_SEQUENCE<int>, FUSION_SEQUENCE<float> >
+    >();
+    test_all<
+        FUSION_SEQUENCE<FUSION_SEQUENCE<int, float>, FUSION_SEQUENCE<float> >
+    >();
+    test_all<
+        FUSION_SEQUENCE<FUSION_SEQUENCE<int>, FUSION_SEQUENCE<float, int> >
+    >();
+    test_all<
+        FUSION_SEQUENCE<
+            FUSION_SEQUENCE<int, float>, FUSION_SEQUENCE<float, int>
+        >
+    >();
 }
 

commit ecfccd8eac3d5252eccbe6d9f09d86cf67cfd6fa

Fix failing "boost::fusion::make_vector(boost::phoenix::val(1));"
regression introduced in 79d8e9d11c07099bdf1db0724e8fb1ee92e98920

include/boost/fusion/container/vector/vector.hpp
@@ -75,10 +75,13 @@ namespace boost { namespace fusion
             Sequence
           , This
           , typename enable_if<traits::is_sequence<Sequence>>::type
-        >   : mpl::equal_to<
-                  fusion::result_of::size<Sequence>
-                , fusion::result_of::size<This>
-              >
+          >  : mpl::and_<
+                   mpl::equal_to<fusion::result_of::size<Sequence>, fusion::result_of::size<This> >
+                 , mpl::or_<
+                       mpl::not_<mpl::equal_to<fusion::result_of::size<This>, mpl::int_<1> >  >,
+                       mpl::not_<boost::is_convertible<Sequence, typename result_of::value_at_c<This, 0>::type> >
+                   >
+               >
         {
         };
 
@@ -326,4 +329,3 @@ namespace boost { namespace fusion
 
 #endif
 #endif
-