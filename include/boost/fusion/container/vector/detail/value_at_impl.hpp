e962c1abb5312df23868a83ac66dd9da9773ca5f
Kohei Takahashi <flast@flast.jp>
2018-04-18 22:31:35 +0900

    Fixed vector compile error with C-style array

--- include/boost/fusion/container/vector/detail/value_at_impl.hpp
+++ include/boost/fusion/container/vector/detail/value_at_impl.hpp
@@ -23,6 +23,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <boost/fusion/container/vector/vector_fwd.hpp>
 #include <boost/type_traits/declval.hpp>
+#include <boost/mpl/identity.hpp>
 
 namespace boost { namespace fusion
 {
@@ -35,7 +36,7 @@ namespace boost { namespace fusion
 
         template <std::size_t N, typename U>
         static inline BOOST_FUSION_GPU_ENABLED
-        U value_at_impl(store<N, U> const volatile*);
+        mpl::identity<U> value_at_impl(store<N, U> const volatile*);
     }
 
     namespace extension
@@ -49,8 +50,8 @@ namespace boost { namespace fusion
             template <typename Sequence, typename N>
             struct apply
             {
-                typedef
-                    decltype(vector_detail::value_at_impl<N::value>(boost::declval<Sequence*>()))
+                typedef typename
+                    decltype(vector_detail::value_at_impl<N::value>(boost::declval<Sequence*>()))::type
                 type;
             };
         };
