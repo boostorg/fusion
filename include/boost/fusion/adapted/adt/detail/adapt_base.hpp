e29b1a07f346a1a69c05e5a4851cc0c6e21ac8cf
Kohei Takahashi <flast@flast.jp>
2018-07-28 14:49:50 +0900

    adapt: Cleanup includes

--- include/boost/fusion/adapted/adt/detail/adapt_base.hpp
+++ include/boost/fusion/adapted/adt/detail/adapt_base.hpp
@@ -23,6 +23,7 @@
 #include <boost/type_traits/is_const.hpp>
 #include <boost/type_traits/add_const.hpp>
 #include <boost/type_traits/remove_const.hpp>
+#include <boost/type_traits/remove_reference.hpp>
 
 #include <boost/typeof/typeof.hpp>
 
