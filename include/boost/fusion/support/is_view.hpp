19d6c7ad3b4b52eccdb05030bbfa3c4aed54e09f
djowel <djowel@gmail.com>
2019-02-18 13:59:58 +0800

    Fix is_view_impl to return false for non fusion sequences (just like is_sequence_impl). (reverted from commit 1c693f05bfba855c69fe85bcf8a9ebb1823508a7)

--- include/boost/fusion/support/is_view.hpp
+++ include/boost/fusion/support/is_view.hpp
@@ -1,7 +1,7 @@
 /*=============================================================================
     Copyright (c) 2001-2011 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    Distributed under the Boost Software License, Version 1.0. (See accompanying 
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_IS_VIEW_03202006_0015)
@@ -31,13 +31,6 @@ namespace boost { namespace fusion
             {
                 typedef typename T::is_view type;
             };
-        };
-
-        template <>
-        struct is_view_impl<non_fusion_tag>
-        {
-            template <typename T>
-            struct apply : mpl::false_ {};
         };
 
         template <>
