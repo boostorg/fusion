e15016adb6df0be829a238802bead45e8843f531
denzor200 <denismikhaylov38@gmail.com>
2021-11-07 23:07:18 +0400

    Applied fixes for reverse_view

--- include/boost/fusion/view/reverse_view/detail/deref_data_impl.hpp
+++ include/boost/fusion/view/reverse_view/detail/deref_data_impl.hpp
@@ -1,6 +1,6 @@
 /*=============================================================================
     Copyright (c) 2009 Christopher Schmidt
-    Copyright (c) 2021-2022 Denis Mikhailov
+    Copyright (c) 2021 Denis Mikhailov
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,8 +10,7 @@
 #define BOOST_FUSION_VIEW_REVERSE_VIEW_DETAIL_DEREF_DATA_IMPL_HPP
 
 #include <boost/fusion/support/config.hpp>
-#include <boost/fusion/iterator/deref_data.hpp>
-#include <boost/fusion/iterator/prior.hpp>
+#include <boost/fusion/view/reverse_view/detail/deref_impl.hpp>
 
 namespace boost { namespace fusion { namespace extension
 {
@@ -20,26 +19,8 @@ namespace boost { namespace fusion { namespace extension
 
     template <>
     struct deref_data_impl<reverse_view_iterator_tag>
-    {
-        template <typename It>
-        struct apply
-        {
-            typedef typename
-                result_of::deref_data<
-                    typename result_of::prior<
-                        typename It::first_type
-                    >::type
-                >::type
-            type;
-
-            BOOST_CONSTEXPR BOOST_FUSION_GPU_ENABLED
-            static type
-            call(It const& it)
-            {
-                return fusion::deref_data(fusion::prior(it.first));
-            }
-        };
-    };
+    : deref_impl<reverse_view_iterator_tag>
+        {};
 }}}
 
 #endif
