96187d23d1dccb98a9f4ba14fc6c20249956434b
niXman <i.nixman@autistici.org>
2022-12-22 17:07:30 +0200

    the test case was added

---
+++ test/support/make_pair_r-value.cpp
@@ -0,0 +1,21 @@
+/*=============================================================================
+    Copyright (c) 2022 niXman (github dot nixman at pm.me)
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#include <boost/fusion/support/pair.hpp>
+
+struct noncopyable_type {
+    noncopyable_type(const noncopyable_type &) = delete;
+    noncopyable_type& operator=(const noncopyable_type &) = delete;
+
+};
+
+int main() {
+    using namespace boost::fusion;
+
+    pair<int, noncopyable_type> val = make_pair<int>(noncopyable_type{});
+
+	return 0;
+}
