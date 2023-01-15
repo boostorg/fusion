a7bb2c7b0f9b256fffa317a6fd5a25bc0272cb22
denzor200 <denismikhaylov38@gmail.com>
2023-01-15 22:55:56 +0600

    [pfr_implicit_explicit] Update copyright years

--- include/boost/fusion/adapted/boost_pfr.hpp
+++ include/boost/fusion/adapted/boost_pfr.hpp
@@ -1,5 +1,5 @@
 /*=============================================================================
-    Copyright (c) 2021-2022 Denis Mikhailov
+    Copyright (c) 2021-2023 Denis Mikhailov
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
